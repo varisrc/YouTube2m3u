e6d32b0fbc2d4d903483f39738b11b54ad91fa4f
varisrc <91376343+varisrc@users.noreply.github.com>
2022-04-05 08:16:04 +0800

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -15,13 +15,13 @@ def grab(url):
         #response = requests.get(url).text
         if '.m3u8' not in response:
             if windows:
-                print('https://raw.githubusercontent.com/varisrc/YouTube2m3u/main/assets/will%20be%20right%20back.mp4')
+                print('https://raw.githubusercontent.com/varisrc/YouTube2m3u/main/assets/YouCut_20220402_155417947.mp4')
                 return
             #os.system(f'wget {url} -O temp.txt')
             os.system(f'curl "{url}" > temp.txt')
             response = ''.join(open('temp.txt').readlines())
             if '.m3u8' not in response:
-                print('https://raw.githubusercontent.com/varisrc/YouTube2m3u/main/assets/will%20be%20right%20back.mp4')
+                print('https://raw.githubusercontent.com/varisrc/YouTube2m3u/main/assets/YouCut_20220402_155417947.mp4')
                 return
     end = response.find('.m3u8') + 5
     tuner = 100
