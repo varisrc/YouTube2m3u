d8fcdf2cf661659a51a6c020e0cdba9ae810c6dd
varisrc <91376343+varisrc@users.noreply.github.com>
2022-04-05 15:24:18 +0800

    Update youtube_m3ugrabber1.py

--- scripts/youtube_m3ugrabber1.py
+++ scripts/youtube_m3ugrabber1.py
@@ -38,6 +38,10 @@ def grab(url):
     st = hd.find('http')
     print(hd[st:].strip())
     #print(f"{link[start : end]}")
+
+print('#EXTM3U')
+print('#EXT-X-VERSION:3')
+print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000')
 s = requests.Session()
 with open('../youtube_channel_info1.txt') as f:
     for line in f:
