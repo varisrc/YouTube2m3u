b6a3be9f7473ceb8e21ea591bbf81c614b30b8f4
varisrc <91376343+varisrc@users.noreply.github.com>
2022-04-19 19:50:13 +0800

    Update sukatv.py

--- scripts/sukatv.py
+++ scripts/sukatv.py
@@ -40,8 +40,6 @@ def grab(url):
     #print(f"{link[start : end]}")
 
 print('#EXTM3U')
-print('#EXT-X-VERSION:3')
-print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=2560000')
 s = requests.Session()
 with open('../YTLink_sukatv.txt') as f:
     for line in f:
